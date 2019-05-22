9c3d5c77a37b1d653b032ffcfb0f9f4e760590d0
honjun <hj531@live.com>
2019-05-22 10:09:56 +0800

    update 注释和捕获异常

--- md_image_backup_py3.py
+++ md_image_backup_py3.py
@@ -14,6 +14,7 @@ def backup():
         # 备份文件夹下的所有img
         search('你的markdown文件路径', '.md')
 
+# example
 # def backup():
 #     try:
 #         download(str('E:\\blog\\hojunBlog19.05.21\\source\\links\\' + sys.argv[1]))
@@ -40,17 +41,27 @@ def download(file_path):
     text = f_md.read().decode('utf-8')
     # regex
     # img_reg = r'\!{1}\[(.*?)\]\((.*?)\)'
+    # 匹配封面图
+    # photos: 
+    #  - imgurl
     # result = re.findall('photos\:\n \- (.*?)\n', text)
+    # 匹配封面图 photos: imgurl
     # result = re.findall('photos\: (.*?)\n', text)
+    # 匹配fancybox插件语法 {% fb_img imgurl desc %}
     # result = re.findall('\{\% fb_img (.*?) ', text)
+    # 匹配html <img src="imgurl">
     # result = re.findall('src\=\"(.*?)\"', text)
+    # 匹配markdown ![XXX](imgurl)
     result = re.findall('!\[(.*?)\]\((.*?)\)', text)
     
     print(result)
     try:
         for i in range(len(result)):
+            # markdown 有两个匹配用这个
             img_quote = result[i][0]
             img_url = result[i][1]
+            # 其他的只有一个匹配用这个 注意之后的代码要哦删掉img_quote
+            # img_url = result[i]
             # download img
             request = urllib.request.Request(img_url)
             response = urllib.request.urlopen(request)
